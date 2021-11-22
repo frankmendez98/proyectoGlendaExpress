ea50c64a8e47fc6c071bcc4788206e2027bd1219
heisenberg <frank.01.1998.fm@gmail.com>
2021-11-21 22:36:49 -0600

    PRE-30 Actualizacion de interfaz para administrar ordenes online

--- app/Views/paquetes_online/admin.php
+++ app/Views/paquetes_online/admin.php
@@ -34,7 +34,10 @@
                                                 <tr>
                                                     <th>ID</th>
                                                     <th style="width: 60%;">Seguimiento</th>
-                                                    <th style="width: 20%;">Accion</th>
+                                                    <th style="width: 10%;">Cantidad</th>
+                                                    <th style="width: 10%;">Precio</th>
+                                                    <th style="width: 10%;">Fecha Ent.</th>
+                                                    <th style="width: 10%;">Accion</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -45,6 +48,9 @@
                                                         <tr>
                                                             <td><?=$arrPaquete->id;?></td>
                                                             <td><?=$arrPaquete->numero_seguimiento;?></td>
+                                                            <td><?=$arrPaquete->cantidad;?></td>
+                                                            <td><?=$arrPaquete->precio_pedido;?></td>
+                                                            <td><?=$arrPaquete->fecha_entrega;?></td>
                                                             <td>
                                                                 <div class="dropdown">
                                                                     <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
