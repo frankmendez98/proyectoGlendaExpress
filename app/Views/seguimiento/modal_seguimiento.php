32d258abc7ed5d6bef63656331e52fe74a93a2de
jhonatan7997 <jonathansosa7997@gmail.com>
2021-11-24 19:41:22 -0600

    PRE-24 actualizacion del modulo de seguimiento

--- app/Views/seguimiento/modal_seguimiento.php
+++ app/Views/seguimiento/modal_seguimiento.php
@@ -7,7 +7,7 @@ $dat = uniqid();
     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
   </div>
   <div class="col-lg-11">
-    <h5 class="modal-title">Mesas</h5>
+    <h5 class="modal-title">Seguimiento de Paquete</h5>
     <small class="font-bold"></small>
   </div>
 </div>
@@ -31,7 +31,7 @@ $dat = uniqid();
     <div class="col-lg-6">
         <div class="form-group">
             <label for="">Observacion</label>
-            <input type="text" class="form-control" name="observacion" id="observacion">
+            <input type="text" class="form-control select2" name="observacion" id="observacion">
         </div>
     </div>
   </div>
@@ -41,3 +41,11 @@ $dat = uniqid();
     <button type="button" class="btn btn-primary" id="agregarSeguimientoModal" data-dismiss="modal">Agregar</button>
     <button type="button" class="btn btn-white closingdata" data-dismiss="modal">Cerrar</button>
 </div>
+
+<script>
+  $(document).ready(function(){
+    $(".select2").select2({
+    dropdownParent: $("#productMo")
+    });
+  })
+</script>
