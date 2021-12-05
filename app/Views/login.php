620e7b222b88f1b0682c70bd1170f069c04ab43f
JHookThat <miguelguzz1995@gmail.com>
2021-12-04 18:41:02 -0600

    PRE-32 creacion de interfaz para el login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,1 +1,48 @@
-login
+<link href="<?=base_url('public/assets/css/bootstrap.min.css');?>" rel="stylesheet">
+    <link href="<?= base_url('public/assets/css/datatables.min.css'); ?>" rel="stylesheet">
+
+    <link href="<?=base_url('public/assets/font-awesome/css/font-awesome.css');?>" rel="stylesheet">
+
+    <!-- Toastr style -->
+    <link href="<?=base_url('public/assets/css/plugins/toastr/toastr.min.css');?>" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="<?=base_url('public/assets/js/plugins/gritter/jquery.gritter.css');?>" rel="stylesheet">
+
+    <link href="<?=base_url('public/assets/css/animate.css');?>" rel="stylesheet">
+    <link href="<?=base_url('public/assets/css/style.css');?>" rel="stylesheet">
+	<link href="<?= base_url('public/assets/libs/izitoast/iziToast.min.css'); ?>" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url('public/assets/libs/cropper/cropper.min.css'); ?>" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url('public/assets/libs/dataTables/dataTables.css'); ?>" rel="stylesheet">
+	<link href="<?= base_url('public/assets/libs/select2/select2.min.css'); ?>" rel="stylesheet">
+	<link href="<?= base_url('public/assets/libs/sweetalert2/sweetalert2.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url("public/assets/libs/dropify/dropify.min.css"); ?>" rel="stylesheet">
+    <link href="<?= base_url("public/assets/libs/datapicker/boostrap-datepicker.min.css"); ?>" rel="stylesheet">
+
+<div id="wrapper">
+    <div id="page-wrapper" class="contenedor_principal gray-bg container">
+        <div class="wrapper wrapper-content animated fadeInRight">
+            <form action="" id="form_edit">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <h5>Datos Orden Online</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row">
+                            
+                        </div>
+                        
+                        <div class="row">
+                            <div class="form-actions col-lg-12">
+                                <button type="submit" id="btn_add" name="btn_add" class="btn btn-success m-t-n-xs float-right"><i class="fa fa-upload"></i>
+                                    Actualizar Registro
+                                </button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>           
+            </form>
+        </div>
+    </div>
+</div>
